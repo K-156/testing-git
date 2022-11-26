792e73f97323ee03f82c87df05a3788f41e7453c
K-156 <limstephanie156@gmail.com>
2022-11-26 12:58:20 +0800

    finish navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import Landing from "./pages/Landing";
+import Navbar from "./utils/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Jobify</h1>
       <Landing />
     </div>
